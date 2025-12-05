adc55bef620e0a44d79435c299f3acf171418ecd
Jesucrypto06 <juseco06@gmail.com>
2025-12-05 14:49:35 -0500

    version con lectura y base de datos

--- bd_conexion_pin.py
+++ bd_conexion_pin.py
@@ -1,27 +1,41 @@
-from grovepi import *
-from grove_rgb_lcd import *
-from time import sleep
-from math import isnan
-import mysql.connector
-from mysql.connector import Error
+setRGB(0, 255, 0)
 
-dht_sensor_port = 7 
-dht_sensor_type = 0  # 0 = blue sensor
-
-def conectar_db():
+while True:
     try:
-        conexion = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="sensores"
-        )
-        if conexion.is_connected():
-            print("Conectado a MariaDB")
-        return conexion
-    except Error as e:
-        print("Error conectando a MariaDB:", e)
-        return None
+        [temp, hum] = dht(dht_sensor_port, dht_sensor_type)
+        print("temp =", temp, "C\thumidity =", hum, "%")
 
-conexion = conectar_db()
-cursor = conexion.cursor() if conexion else None
+        # Validación valores
+        if isnan(temp) or isnan(hum):
+            raise TypeError("nan error")
+
+        # Mostrar en LCD
+        setText_norefresh("Temp:" + str(temp) + "C\nHum:" + str(hum) + "%")
+
+        # ---- Guardar en la base de datos ----
+        if conexion is None or not conexion.is_connected():
+            print("Reintentando conexión...")
+            conexion = conectar_db()
+            cursor = conexion.cursor() if conexion else None
+
+        if conexion and conexion.is_connected():
+            sql = "INSERT INTO dht11 (temperatura, humedad) VALUES (%s, %s)"
+            valores = (float(temp), float(hum))
+            cursor.execute(sql, valores)
+            conexion.commit()
+            print(" Datos guardados en la base de datos")
+
+    except (IOError, TypeError) as e:
+        print("Error:", e)
+        setText("")
+
+    except KeyboardInterrupt:
+        print("Programa detenido.")
+        setText("")
+        break
+
+    except Error as e:
+        print("Error en MariaDB:", e)
+        conexion = None  
+
+    sleep(0.05)
